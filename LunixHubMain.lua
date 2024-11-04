ae951213f826e8c381e3b49aabaf1f124b13a3bf
Twolunix <opoi03862@gmail.com>
2024-11-04 12:05:18 -0300

    LunixHubMain.lua

---
+++ LunixHubMain.lua
@@ -0,0 +1,25 @@
+-- Lunix Hub Loading 😈
+-- Ant-Afk ❄️
+    game:GetService("Players").LocalPlayer.Idled:connect(function()
+     	game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+		wait(1)
+		game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+	end)
+ print("Ant-Afk")
+-- Copy Discord ⛩️
+setclipboard("Lunix Hub")
+-- List Of Games Script 👾
+local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
+MenuUI:WindowCreate("Lunix Hub","rbxassetid://80086118575102","Lunix Hub")
+MenuUI:ButtonAdd("Blox Fruits Main",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Twolunix/LunixHub/refs/heads/main/LunixHub.lua"))()
+    MenuUI:WindowDelete()
+end)
+MenuUI:ButtonAdd("Blox Fruits Kaitun",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Twolunix/LunixHub/refs/heads/main/LunixHubKaitun.lua"))()
+    MenuUI:WindowDelete()
+end)
+MenuUI:ButtonAdd("Murder Mystery 2",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Twolunix/LunixHub/refs/heads/main/LunixHubMm2.lua"))()
+    MenuUI:WindowDelete()
+end)
